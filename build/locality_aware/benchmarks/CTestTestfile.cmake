1ab015f7f84a9556d6278649069d95b29780009d
aworley16 <apworley@gmail.com>
2025-07-14 09:06:50 -0500

    removed build, added changes to attempt fix Spack path issue

--- build/locality_aware/benchmarks/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aworley/MPI_advance/mpi-advance/locality_aware/benchmarks
-# Build directory: /home/aworley/MPI_advance/mpi-advance/build/locality_aware/benchmarks
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/locality_aware/src/collective/tests/CMakeFiles/test_suitesparse_alltoallv.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for test_suitesparse_alltoallv.
-# This may be replaced when dependencies are built.

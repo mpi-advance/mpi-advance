d650846bead0863e12c9ef32ecaaabc431b4ddd7
aworley16 <apworley@gmail.com>
2025-09-25 13:13:28 -0500

    confirmed build from repo

--- build/MPIPCL/include/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/apworley/mpiadvanced/meta/mpi-advance/MPIPCL/include
-# Build directory: /home/apworley/mpiadvanced/meta/mpi-advance/build/MPIPCL/include
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/MPIPCL/src/core/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/apworley/mpiadvanced/meta/mpi-advance/MPIPCL/src/core
-# Build directory: /home/apworley/mpiadvanced/meta/mpi-advance/build/MPIPCL/src/core
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("pt2pt")
